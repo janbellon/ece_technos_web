901523652ad5bdcd6e5648a09ba4db8c27c776e5
Jan Bellon <jan@nobell.fr>
2026-09-11 12:29:29 +0200

    Fetching

--- projet1/index.js
+++ projet1/index.js
@@ -2,9 +2,23 @@ import express from "express"
 
 const app = express();
 app.use(express.json())
+app.use(express.text())
 
-app.get("/", (_, res) => {
-    res.status(404).json({"error": "Not found"});
-}) 
+app.get("/myip", (req, res) => {
+    res.status(200).json({"ip": req.ip});
+    console.log('%s -> /myip', req.ip)
+})
 
-app.listen(3000, () => console.log("Listening on http://0.0.0.0:3000"))
+app.get("/id/:id", (req, res) => {
+    res.status(200).json({"id": req.params.id});
+    console.log('%s -> /%s', req.ip, req.params.id)
+})
+
+app.get("/fetch", async (req, res) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const data = await response.json()
+    res.status(200).json(data)
+    console.log('%s -> /fetch', req.ip)
+})
+
+app.listen(3000, () => console.log("Listening on http://0.0.0.0:3000"));
